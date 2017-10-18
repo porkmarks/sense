38c57758e4c5ffaf56a4cccf0d41b9f0649b0fd5
jeanleflambeur <catalin.vasile@gmail.com>
2017-10-18 09:16:58 +0200

    Fixed email

--- manager/src/Emailer.h
+++ manager/src/Emailer.h
@@ -28,9 +28,9 @@ public:
     {
         enum class Connection
         {
-            Tcp,
             Ssl,
-            Tls
+            Tls,
+            Tcp,
         };
 
         std::string host;
