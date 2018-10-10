26125ad2934174a8cb9b3a436fcf3640d6ef3848
jeanleflambeur <catalin.vasile@gmail.com>
2018-10-10 23:42:20 +0200

    Added file for log error

--- base/src/Log.h
+++ base/src/Log.h
@@ -7,5 +7,5 @@
 extern std::string time_point_to_string(std::chrono::system_clock::time_point tp);
 
 #define LOGI std::cout << time_point_to_string(std::chrono::system_clock::now()) << ": "
-#define LOGE std::cerr << time_point_to_string(std::chrono::system_clock::now()) << ": "
+#define LOGE std::cerr << time_point_to_string(std::chrono::system_clock::now()) << ": " << __FILE__ << ":" << __LINE__ << ":: "
 
