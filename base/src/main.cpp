9fb726a004dc815869d52280328985bd9f88cb4a
jeanleflambeur <catalin.vasile@gmail.com>
2018-11-18 22:37:50 +0100

    Led support

--- base/src/main.cpp
+++ base/src/main.cpp
@@ -33,15 +33,143 @@ extern std::string time_point_to_string(std::chrono::system_clock::time_point tp
 constexpr uint32_t RED_LED_PIN = 4;
 constexpr uint32_t GREEN_LED_PIN = 17;
 
-void fatal_error()
+enum class Led_Color
 {
-    gpioWrite(GREEN_LED_PIN, 0);
-    while (true)
+    None,
+    Green,
+    Red,
+    Yellow,
+};
+
+enum class Led_Blink
+{
+    None,
+    Slow_Green,
+    Slow_Red,
+    Slow_Yellow,
+    Fast_Green,
+    Fast_Red,
+    Fast_Yellow,
+};
+
+Led_Color s_led_color = Led_Color::None;
+std::atomic<Led_Blink> s_led_blink;
+std::chrono::system_clock::time_point s_led_blink_time_point;
+
+void set_led_color(Led_Color color)
+{
+    if (color == Led_Color::None)
     {
         gpioWrite(RED_LED_PIN, 0);
-        chrono::delay(chrono::millis(200));
+        gpioWrite(GREEN_LED_PIN, 0);
+    }
+    else if (color == Led_Color::Green)
+    {
+        gpioWrite(RED_LED_PIN, 0);
+        gpioWrite(GREEN_LED_PIN, 1);
+    }
+    else if (color == Led_Color::Red)
+    {
         gpioWrite(RED_LED_PIN, 1);
-        chrono::delay(chrono::millis(200));
+        gpioWrite(GREEN_LED_PIN, 0);
+    }
+    else if (color == Led_Color::Yellow)
+    {
+        gpioWrite(RED_LED_PIN, 1);
+        gpioWrite(GREEN_LED_PIN, 1);
+    }
+    s_led_color = color;
+}
+
+void led_thread_func()
+{
+    gpioSetMode(RED_LED_PIN, PI_OUTPUT);
+    gpioSetMode(GREEN_LED_PIN, PI_OUTPUT);
+    gpioWrite(RED_LED_PIN, 0);
+    gpioWrite(GREEN_LED_PIN, 0);
+
+    while (true)
+    {
+        auto now = std::chrono::system_clock::now();
+        if (s_led_blink_time_point > now && s_led_blink != Led_Blink::None)
+        {
+            if (s_led_blink == Led_Blink::Slow_Red)
+            {
+                gpioWrite(RED_LED_PIN, 0);
+                gpioWrite(GREEN_LED_PIN, 0);
+                chrono::delay(chrono::millis(100));
+                gpioWrite(RED_LED_PIN, 1);
+                gpioWrite(GREEN_LED_PIN, 0);
+                chrono::delay(chrono::millis(100));
+            }
+            else if (s_led_blink == Led_Blink::Slow_Green)
+            {
+                gpioWrite(RED_LED_PIN, 0);
+                gpioWrite(GREEN_LED_PIN, 0);
+                chrono::delay(chrono::millis(100));
+                gpioWrite(RED_LED_PIN, 0);
+                gpioWrite(GREEN_LED_PIN, 1);
+                chrono::delay(chrono::millis(100));
+            }
+            else if (s_led_blink == Led_Blink::Slow_Yellow)
+            {
+                gpioWrite(RED_LED_PIN, 0);
+                gpioWrite(GREEN_LED_PIN, 0);
+                chrono::delay(chrono::millis(100));
+                gpioWrite(RED_LED_PIN, 1);
+                gpioWrite(GREEN_LED_PIN, 1);
+                chrono::delay(chrono::millis(100));
+            }
+            else if (s_led_blink == Led_Blink::Fast_Red)
+            {
+                gpioWrite(RED_LED_PIN, 0);
+                gpioWrite(GREEN_LED_PIN, 0);
+                chrono::delay(chrono::millis(50));
+                gpioWrite(RED_LED_PIN, 1);
+                gpioWrite(GREEN_LED_PIN, 0);
+                chrono::delay(chrono::millis(50));
+            }
+            else if (s_led_blink == Led_Blink::Fast_Green)
+            {
+                gpioWrite(RED_LED_PIN, 0);
+                gpioWrite(GREEN_LED_PIN, 0);
+                chrono::delay(chrono::millis(50));
+                gpioWrite(RED_LED_PIN, 0);
+                gpioWrite(GREEN_LED_PIN, 1);
+                chrono::delay(chrono::millis(50));
+            }
+            else if (s_led_blink == Led_Blink::Fast_Yellow)
+            {
+                gpioWrite(RED_LED_PIN, 0);
+                gpioWrite(GREEN_LED_PIN, 0);
+                chrono::delay(chrono::millis(50));
+                gpioWrite(RED_LED_PIN, 1);
+                gpioWrite(GREEN_LED_PIN, 1);
+                chrono::delay(chrono::millis(50));
+            }
+        }
+        else
+        {
+            set_led_color(s_led_color);
+            chrono::delay(chrono::millis(10));
+        }
+    }
+}
+
+void set_led_blink(Led_Blink blink, std::chrono::system_clock::duration duration, bool force = false)
+{
+    auto now = std::chrono::system_clock::now();
+    if (now >= s_led_blink_time_point || s_led_blink == Led_Blink::None || force)
+    {
+        s_led_blink = blink;
+        s_led_blink_time_point = std::chrono::system_clock::now() + duration;
+    }
+}
+
+void fatal_error()
+{
+    while (true)
+    {
     }
 }
 
@@ -53,12 +181,7 @@ int main(int, const char**)
 
     gpioInitialise();
 
-    {
-        gpioSetMode(RED_LED_PIN, PI_OUTPUT);
-        gpioSetMode(GREEN_LED_PIN, PI_OUTPUT);
-        gpioWrite(RED_LED_PIN, 0);
-        gpioWrite(GREEN_LED_PIN, 1);
-    }
+    std::thread led_thread(&led_thread_func);
 
     size_t tries = 0;
     while (!s_sensor_comms.init(10, 20))
@@ -68,12 +191,11 @@ int main(int, const char**)
         std::this_thread::sleep_for(std::chrono::seconds(1));
         if (tries > 10)
         {
+            set_led_blink(Led_Blink::Fast_Red, std::chrono::hours(24 * 1000), true);
             fatal_error();
             return EXIT_FAILURE;
         }
     }
-    gpioWrite(RED_LED_PIN, 0);
-    gpioWrite(GREEN_LED_PIN, 1);
 
     s_sensor_comms.set_address(Sensor_Comms::BASE_ADDRESS);
 
@@ -90,18 +212,32 @@ int main(int, const char**)
     if (!s_server.init(4444, 5555))
     {
         LOGE << "Server init failed" << std::endl;
+        set_led_blink(Led_Blink::Fast_Yellow, std::chrono::hours(24 * 1000), true);
         fatal_error();
         return EXIT_FAILURE;
     }
+
+    set_led_color(Led_Color::Green);
 
     Server::Sensor_Request request;
     Server::Sensor_Response response;
     while (true)
     {
+        if (s_server.is_connected())
+        {
+            set_led_color(Led_Color::Green);
+        }
+        else
+        {
+            set_led_blink(Led_Blink::Slow_Green, std::chrono::seconds(10));
+        }
+
         uint8_t size = Sensor_Comms::MAX_USER_DATA_SIZE;
         uint8_t* packet_data = s_sensor_comms.receive_packet(raw_packet_data.data(), size, 1000);
         if (packet_data)
         {
+            set_led_color(Led_Color::Yellow);
+
             request.type = s_sensor_comms.get_rx_packet_type(packet_data);
             request.signal_s2b = s_sensor_comms.get_input_dBm();
             request.address = s_sensor_comms.get_rx_packet_source_address(packet_data);
@@ -121,7 +257,8 @@ int main(int, const char**)
             {
                 memcpy(request.payload.data(), s_sensor_comms.get_rx_packet_payload(packet_data), size);
             }
-            if (s_server.send_sensor_message(request, response))
+            Server::Result result = s_server.send_sensor_message(request, response);
+            if (result == Server::Result::Has_Response)
             {
                 s_sensor_comms.set_destination_address(response.address);
                 s_sensor_comms.begin_packet(raw_packet_data.data(), response.type);
@@ -131,8 +268,14 @@ int main(int, const char**)
                 }
                 if (!s_sensor_comms.send_packet(raw_packet_data.data(), response.retries))
                 {
+                    set_led_blink(Led_Blink::Slow_Red, std::chrono::seconds(1), true);
                     LOGE << "Sensor comms failed" << std::endl;
                 }
+            }
+            else if (result != Server::Result::Ok)
+            {
+                LOGE << "Server comms failed: " << int(result) << std::endl;
+                set_led_blink(Led_Blink::Fast_Red, std::chrono::seconds(1), true);
             }
         }
         else
